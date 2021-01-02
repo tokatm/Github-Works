637d847c2c6a7b9d67aecae0a7c3787784f11b43
mustafatokat <mstf.tokat@gmail.com>
2021-01-02 19:37:21 +0300

    completed

--- An example for DES/DES Exapmle.py
+++ An example for DES/DES Exapmle.py
@@ -109,11 +109,39 @@ PC_2 = [14, 17, 11, 24, 1, 5, 3, 28,
 SHIFT_TABLE = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
 
 #%% get and convert to binary plaintext and key
-plainText = "CalmDown" #input(str("Plaintext giriniz: "))
-key = "CryptoEn" #input(str("Anahtar giriniz: "))
+plainText = input(str("Plaintext giriniz: "))
+key = input(str("Anahtar giriniz: "))
 
 pTexttoBin = ''.join(format(ord(i), 'b').zfill(8) for i in plainText)
 keytoBin = ''.join(format(ord(i), 'b').zfill(8) for i in key)
+
+def bin2hex(s): 
+    mp = {"0000" : '0',  
+          "0001" : '1', 
+          "0010" : '2',  
+          "0011" : '3', 
+          "0100" : '4', 
+          "0101" : '5',  
+          "0110" : '6', 
+          "0111" : '7',  
+          "1000" : '8', 
+          "1001" : '9',  
+          "1010" : 'A', 
+          "1011" : 'B',  
+          "1100" : 'C', 
+          "1101" : 'D',  
+          "1110" : 'E', 
+          "1111" : 'F' } 
+    hex = "" 
+    for i in range(0,len(s),4): 
+        ch = "" 
+        ch = ch + s[i] 
+        ch = ch + s[i + 1]  
+        ch = ch + s[i + 2]  
+        ch = ch + s[i + 3]  
+        hex = hex + mp[ch] 
+          
+    return hex
 
 #%% required functions
 
@@ -235,7 +263,8 @@ for i in range(0,16):
     round_key = permute(combineKey, PC_2, 48)
 
     roundkey.append(round_key)
-cipher_Text =encrypt(pTexttoBin, roundkey)
+cipher_Text = encrypt(pTexttoBin, roundkey)
+cipher_Text = bin2hex(cipher_Text)
 print("plaintext",pTexttoBin)
 print("key",keytoBin)
 print("Ciphertext: ", cipher_Text)
